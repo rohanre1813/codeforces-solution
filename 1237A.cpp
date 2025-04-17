d3ae3d25d14c06ea2b79b388d576bb5e7d882051
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-17 18:30:03 +0000

    contest: Codeforces Global Round 5, problem: (A) Balanced Rating Changes,

---
+++ 1237A.cpp
@@ -0,0 +1,31 @@
+// contest: Codeforces Global Round 5, problem: (A) Balanced Rating Changes
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int a[n],ar[n],sum=0;
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+    ar[i]=a[i]/2;
+    sum+=ar[i];
+   }
+ for(int j=0;j<n;j++){
+    if(a[j]%2!=0){
+     if(sum<0 && a[j]>0){
+        ar[j]++;
+        sum++;
+     }
+     else if(sum>0 && a[j]<0){
+         ar[j]--;
+         sum--;
+     }
+    }
+ }
+  for(auto i:ar){
+    cout<<i<<endl;
+ }
+}
+
