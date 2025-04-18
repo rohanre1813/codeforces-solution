e417454bc58ac2e9e5a55f5d60516ed17cb1b660
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-18 13:37:15 +0000

    A. Ichihime and Triangle

---
+++ striver's cp sheet/maths/1337A.cpp
@@ -0,0 +1,15 @@
+// A. Ichihime and Triangle
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  long long a,b,c,d;
+  cin>>t;
+  while(t--){
+    cin>>a>>b>>c>>d;
+    cout<<b<<" "<<c<<" "<<c<<endl;
+  }
+}
+
