aa56c5bbd433ae41ecaa15e045664ce2fbc1b4ab
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-18 13:40:50 +0000

    A - Mahmoud and Ehab and the even-odd game

---
+++ striver's cp sheet/maths/959A.cpp
@@ -0,0 +1,12 @@
+// A - Mahmoud and Ehab and the even-odd game
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long n;
+  cin>>n;
+  if(n%2==1){cout<<"Ehab";}
+  else {cout<<"Mahmoud";}   
+  }
+
