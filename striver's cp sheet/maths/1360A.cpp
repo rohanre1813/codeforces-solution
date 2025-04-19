60f51ff9f7e2ada64227e881a328a587835aca1d
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-19 11:45:43 +0000

    A. Minimal Square

---
+++ striver's cp sheet/maths/1360A.cpp
@@ -0,0 +1,19 @@
+// A. Minimal Square
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int a,b;
+        cin>>a>>b;
+       
+      cout<<pow(max(min(a,b)*2,max(a,b)),2)<<endl;
+    }
+    return 0;
+}
