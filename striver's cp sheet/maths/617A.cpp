6f45bbdd378444330ee8bc37efc2f8a8af267f7e
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-23 14:16:45 +0000

    A. Elephant

---
+++ striver's cp sheet/maths/617A.cpp
@@ -0,0 +1,21 @@
+// A. Elephant
+
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int x;
+        cin>>x;
+        cout<<x/5 +(x%5!=0);
+        
+    }
+    return 0;
+}
