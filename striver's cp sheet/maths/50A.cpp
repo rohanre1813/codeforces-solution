d273278f01f2287a0caf8fd9810fe8288b8d150c
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-05-17 03:46:34 +0000

    A. Domino piling

---
+++ striver's cp sheet/maths/50A.cpp
@@ -0,0 +1,18 @@
+//A. Domino piling
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        cout<<(n*m)/2<<endl;
+    }
+    return 0;
+}
